4ada6c2dabc9fadc38a4dab4b42d7fccf9f98d70
tjmanoj <manojtofficial@gmail.com>
2023-12-03 13:53:58 +0530

    day 1 completed

--- day01/main.py
+++ day01/main.py
@@ -1,4 +1,4 @@
-print("Welcome to the brand generator!")
-city_name = input("What city did you grew up in?")
-pet_name = input("What is the name of your first pet?")
+print("Welcome to the brand generator!\n")
+city_name = input("What city did you grew up in?\n")
+pet_name = input("What is the name of your first pet?\n")
 print("Your brand name is " + city_name + " " + pet_name)
