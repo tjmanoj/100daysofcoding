7d68f74d8b50de1aa5c107eb30e32fa10cb6480b
tjmanoj <manojtofficial@gmail.com>
2023-12-03 13:50:21 +0530

    day 1 completed

--- day01/main.py
+++ day01/main.py
@@ -1,4 +1,4 @@
-print("Welcome to The Band Name Generator!")
-city = input("What city did you grow up in?\n")
-pet = input("What is the name of your first pet?\n")
-print("Your band name is " + city +  " " + pet)
+print("Welcome to the brand generator!")
+city_name = input("What city did you grew up in?")
+pet_name = input("What is the name of your first pet?")
+print("Your brand name is " + city_name + " " + pet_name)
