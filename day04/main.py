5408a38f0ea1c8e1490a89a25a116ae266d170e3
tjmanoj <manojtofficial@gmail.com>
2023-12-04 07:46:33 +0530

    day 4 completed

--- day04/main.py
+++ day04/main.py
@@ -1,6 +1,3 @@
-import random
-
-
 rock = '''
     _______
 ---'   ____)
@@ -28,47 +25,32 @@ scissors = '''
 ---.__(___)
 '''
 
+#Write your code below this line 👇
+import random
+print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+list1 = ['Rock','Paper','Scissors']
+list2 = [rock,paper,scissors]
+user_choice = int(input())
+computer_choice = random.randint(0,2)
+print(f"Your choice: {list1[user_choice]}")
+print(list2[user_choice])
+print(f"Computer choice: {list1[computer_choice]}")
+print(list2[computer_choice])
 
-choice = (input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n0/1/2:"))
-computers_choice = random.randint(0,2)
-
-if not choice.isdigit():
-  print("You've entered an invalid value, try again and choose a number between 0-2.")
+if user_choice == computer_choice:
+    print("It\'s a draw!")
+elif user_choice == 0 and computer_choice == 1:
+    print("You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif user_choice == 1 and computer_choice == 0:
+    print("You win!")
+elif user_choice == 1 and computer_choice == 2:
+    print("You lose!")
+elif user_choice == 2 and computer_choice == 0:
+    print("You lose!")
+elif user_choice == 2 and computer_choice == 1:
+    print("You win!")
 else:
-  choice = int(choice)
-  if choice > 2:
-    print("You've entered an invalid number, try again and choose a number between 0-2.")
-  elif choice == 0 or choice == 1 or choice == 2:
-    if choice == 0:
-      print(f"You chose: Rock {rock}")
-    elif choice == 1:
-      print(f"You chose: Paper {paper}")
-    elif choice == 2:
-      print(f"You chose: Scissors {scissors}")
-
-    if computers_choice == 0:
-      print(f"The computer chose: Rock {rock}")
-      if choice == 2:
-        print("You lose, Rock wins against scissors.")
-      elif choice == 0:
-        print("It's a draw!")
-      else:
-        print("You win!")
-
-    elif computers_choice == 1:
-      print(f"The computer chose: Paper {paper}")
-      if choice == 0:
-        print("You lose, Paper wins against rock.")
-      elif choice == 0:
-        print("It's a draw!")
-      else:
-        print("You win!")
-
-    elif computers_choice == 2:
-      print(f"The computer chose: Scissors {scissors}")
-      if choice == 1:
-        print("You lose, Scissors win against paper.")
-      elif choice == 2:
-        print("It's a draw!")
-      else:
-        print("You win!")
+    print("invalid choice")
+    
