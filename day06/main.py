0d46ecdb60465921fffe68df8b986ac5efbc020d
tjmanoj <manojtofficial@gmail.com>
2023-12-06 22:49:19 +0530

    day 6 completed

--- day06/main.py
+++ day06/main.py
@@ -1,17 +1,18 @@
-def turn_right():    
+def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
 while front_is_clear():
     move()
 turn_left()
     
+
 while not at_goal():
     if right_is_clear():
         turn_right()
         move()
     elif front_is_clear():
         move()
-    else:
+    elif wall_in_front() or wall_on_right():
         turn_left()
+        
