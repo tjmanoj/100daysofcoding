27bfc3dd3804b09ebe69243cf5352117fdf3e0e8
tjmanoj <manojtofficial@gmail.com>
2023-12-07 12:31:00 +0530

    day 7 completed

--- day07/main.py
+++ day07/main.py
@@ -1,55 +1,47 @@
-from replit import clear
 import random
-from hangman_words import word_list
-from hangman_art import stages, logo2, logo3
+from replit import clear
+import hangman_words as word
+import hangman_art as art
 
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(word.word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
-print(logo3)
-print("\nTo win, guess the word before the person is hung.\n")
+
+print(art.logo)
+
 
 display = []
-wrong_guesses = []
 for _ in range(word_length):
     display += "_"
 
 while not end_of_game:
-    guess = input("Guess a letter:").lower()
-
+    guess = input("Guess a letter: ").lower()
     clear()
+    if guess in display:
+      print(f"You already guessed the letter {guess}")
 
-    if guess in wrong_guesses:
-      print(f"{' '.join(display)}")
-      print(stages[lives])
-      print(f"You've already guessed with the letter '{guess}', pick another letter.")
-    else:
-      wrong_guesses.append(guess)
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 
-      for position in range(word_length):
-          letter = chosen_word[position]
-          if letter == guess:
-              display[position] = letter
 
-      print(f"{' '.join(display)}")
+    if guess not in chosen_word:
+      print(f"You guessed {guess}, that's not in the word. You lose a life.")
 
-      if "_" not in display:
-          end_of_game = True
-          print("\nGeninus, genius, genius! You won!")
-          print(logo2)
-
-      if guess not in chosen_word:
-        lives -= 1
-
-      if not end_of_game:
-        print(stages[lives])
-        if guess not in chosen_word:
-          print(f"'{guess}' is not in the word, you lost 1 life.")
-
+      lives -= 1
       if lives == 0:
         end_of_game = True
-        print("The man has been hung, you lose.")
-        print(f"\nThe word was '{chosen_word}'")
+        print("You lose.")
+
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(art.stages[lives])
