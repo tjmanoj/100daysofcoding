1a4c1618c149e6c8423d10ba01d6b180ba43085a
tjmanoj <manojtofficial@gmail.com>
2023-12-09 10:47:00 +0530

    day 8 completed

--- day08/main.py
+++ day08/main.py
@@ -1,33 +1,30 @@
 from art import logo
 
+print(logo)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    if char.isalpha():
-      position = alphabet.index(char)
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
+def caeser(text,shift,direction):
+  result = ""
+  if direction == "decode":
+    shift *= -1
+  for word in text:
+    if word.isalpha():
+      ind  = (alphabet.index(word) + shift) % 26
+      result += alphabet[ind]
     else:
-      end_text += char
-  print(f"Here's the {cipher_direction}d result: {end_text}\n")
+      result += word
+  print(f"The {direction} text is {result}")
 
-run = True
-while run:
-  print(logo)
+choice = True
+  
+while choice:
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
-
-  if shift > 26:
-    shift = shift % shift == 0
-
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  choice = input("Do you want to run this program again?\nType 'yes' or 'no': ")
-  if choice == 'no':
-    run = False
-    print("Goodbye.")
+  caeser(text,shift,direction)
+  user_choice = input("Type 'yes' if you want to go again, Otherwise type 'no'")
+  if user_choice == "no":
+    choice = False
+    print("Goodbye")
