65664b406c009ac23c45bad63c41e41d131ca983
tjmanoj <manojtofficial@gmail.com>
2023-12-18 23:59:55 +0530

    day 16 completed

--- day16/main.py
+++ day16/main.py
@@ -2,30 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# List of drinks
-espresso = MenuItem("espresso", 50, 0, 18, 1.5)
-latte = MenuItem("latte", 200, 150, 24, 2.5)
-cappuccino = MenuItem("cappuccino", 250, 100, 24, 3.0)
-
-# For ordering drinks and checking if drink is available
 menu = Menu()
+coffee_make = CoffeeMaker()
+money = MoneyMachine()
+is_machine_on = True
 
-# Printing report of all resources, checking if resources are sufficient, making coffee
-coffee_maker = CoffeeMaker()
-
-# Printing report and taking payment
-money_machine = MoneyMachine()
-
-on = True
-while on:
-    choice = input(f"What would you like? Choose between espresso, latte or cappuccino: ")
-    if choice == 'off':
-        print("Have a good one!")
-        on = False
-    elif choice == 'report':
-        coffee_maker.report()
-        money_machine.report()
+while is_machine_on:
+    user_choice = input("What would you like? (espresso/latte/cappuccino):")
+    if user_choice == "off":
+        is_machine_on = False
+    elif user_choice == "report":
+        coffee_make.report()
+        money.report()
     else:
-        coffee_choice = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(espresso) and money_machine.make_payment(espresso.cost):
-            coffee_maker.make_coffee(coffee_choice)
+        drink = menu.find_drink(user_choice)
+        if drink:
+            is_ingredient_available = coffee_make.is_resource_sufficient(drink)
+            if is_ingredient_available:
+                if money.make_payment(drink.cost):
+                    coffee_make.make_coffee(drink)
