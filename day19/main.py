f3d3e59283026fe54868eae5c447428a7831e332
tjmanoj <manojtofficial@gmail.com>
2023-12-29 12:47:34 +0530

    day 19 completed

--- day19/main.py
+++ day19/main.py
@@ -1,34 +1,32 @@
-from turtle import Turtle, Screen
 import random
-
-is_race_on = False
+from turtle import Turtle
+from turtle import Screen
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which color turtle will win the race? Enter a color: ").lower
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_axis = [-100, -60, -20, 20, 60, 100]
-all_turtles = []
+choice = screen.textinput(title="Turtle Game",
+                          prompt="Which turtle will win the race, choose a color?")
 
-for turtle_number in range(0, 6):
-    new_turtle = Turtle(shape="turtle")
-    new_turtle.penup()
-    new_turtle.color(colors[turtle_number])
-    new_turtle.goto(x=-230, y=y_axis[turtle_number])
-    all_turtles.append(new_turtle)
+y = -130
+for color in colors:
+    bubu = Turtle(shape="turtle")
+    bubu.color(color)
+    bubu.penup()
+    bubu.goto(x=-230, y=y)
+    y += 50
+    all_turtles.append(bubu)
 
-if user_bet:
-    is_race_on = True
-
-while is_race_on:
+is_game_over = False
+while not is_game_over:
     for turtle in all_turtles:
-        if turtle.xcor() > 230:
-            winning_color = turtle.pencolor()
-            if winning_color == user_bet:
-                print(f"You've won! The {winning_color} turtle is the winner!")
-            else:
-                print(f"You've lost! The {winning_color} turtle is the winner!")
-            is_race_on = False
-        rand_distance = random.randint(0, 10)
-        turtle.forward(rand_distance)
+        if turtle.xcor() > 230 and choice == turtle.pencolor():
+            print("You're turtle wins")
+            is_game_over = True
+        elif turtle.xcor() > 230:
+            print(f"You lost! The {turtle.pencolor()} turtle wins")
+            is_game_over = True
 
+        random_speed = random.randint(0, 10)
+        turtle.forward(random_speed)
 screen.exitonclick()
