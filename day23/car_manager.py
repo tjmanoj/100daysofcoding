575ea42d982baec2002c48392c2cd9d23ee03935
tjmanoj <manojtofficial@gmail.com>
2024-01-16 19:25:48 +0530

    day 23 intermediate

--- day23/car_manager.py
+++ day23/car_manager.py
@@ -1,7 +1,26 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.all_cars = []
+
+    def create_cars(self):
+        flag = random.randint(1, 5)
+        if flag == 1:
+            car = Turtle("square")
+            car.shapesize(stretch_len=2)
+            car.penup()
+            car.color(random.choice(COLORS))
+            y_range = random.randint(-280, 280)
+            car.goto(280, y_range)
+            self.all_cars.append(car)
+
+    def move(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
--- day23/main.py
+++ day23/main.py
@@ -12,3 +12,26 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car = CarManager()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car.create_cars()
+    car.move()
+
+screen.exitonclick()
